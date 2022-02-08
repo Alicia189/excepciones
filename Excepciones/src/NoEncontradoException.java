ed8178e1390e3e984eb347d0864920295c77cc39
David Casas <davidecasas@hotmail.com>
2022-02-08 12:22:57 +0100

    Commit con el ejercicio 4

---
+++ Excepciones/src/NoEncontradoException.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+
+/**
+ * @author David
+ *
+ */
+public class NoEncontradoException extends Exception {
+
+	/**
+	 * 
+	 */
+	public NoEncontradoException() {
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	/**
+	 * @param message
+	 */
+	public NoEncontradoException(String message) {
+		super(message);
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	/**
+	 * @param cause
+	 */
+	public NoEncontradoException(Throwable cause) {
+		super(cause);
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public NoEncontradoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public NoEncontradoException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+}
